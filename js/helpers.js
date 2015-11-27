af99e64481a451a04a34d280650ac7c4c5f81367
Валентин Немцев <mail@valentin-nemcev.com>
2015-11-27 17:21:01 +0300

    Convert helpers.coffee to js

---
+++ js/helpers.js
@@ -0,0 +1,46 @@
+import keycode from 'keycode';
+
+import * as Transmitter from 'transmitter/index.es';
+
+
+export function getKeycodeMatcher(...keys) {
+  return (keypressPayload) =>
+    keypressPayload
+      .map( (evt) => keys.indexOf(keycode(evt)) >= 0 )
+      .noopIf( (isKey) => !isKey );
+}
+
+
+export class VisibilityToggleVar extends Transmitter.Nodes.Variable {
+
+  constructor($element) {
+    this.$element = $element;
+  }
+
+  set(state) {
+    this.$element.toggle(!!state);
+    return this;
+  }
+
+  get() {
+    return this.$element.is(':visible');
+  }
+}
+
+
+export class ClassToggleVar extends Transmitter.Nodes.Variable {
+
+  constructor($element, _class) {
+    this.$element = $element;
+    this.class = _class;
+  }
+
+  set(state) {
+    this.$element.toggleClass(this.class, !!state);
+    return this;
+  }
+
+  get() {
+    return this.$element.hasClass(this.class);
+  }
+}
