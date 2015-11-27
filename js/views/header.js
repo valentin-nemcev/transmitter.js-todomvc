77f1b89d9a6d5783325418d5cd0c0b545d49560e
Валентин Немцев <mail@valentin-nemcev.com>
2015-11-27 20:10:23 +0300

    Convert header.coffee to js

---
+++ js/views/header.js
@@ -0,0 +1,49 @@
+import * as Transmitter from 'transmitter';
+
+import {getKeycodeMatcher} from '../helpers';
+
+
+const matchEnter = getKeycodeMatcher('enter');
+const matchEscEnter = getKeycodeMatcher('enter', 'esc');
+
+
+export default class HeaderView {
+
+  constructor($element) {
+    this.$element = $element;
+    this.$newTodoInput = this.$element.find('.new-todo');
+
+    this.newTodoLabelInputVar =
+      new Transmitter.DOMElement.InputValueVar(this.$newTodoInput[0]);
+    this.newTodoKeypressEvt =
+      new Transmitter.DOMElement.DOMEvent(this.$newTodoInput[0], 'keyup');
+
+    this.clearNewTodoLabelInputChannel =
+      new Transmitter.Channels.SimpleChannel()
+        .inForwardDirection()
+        .fromSource(this.newTodoKeypressEvt)
+        .toTarget(this.newTodoLabelInputVar)
+        .withTransform(
+          (keypressPayload) => matchEscEnter(keypressPayload).map( () => '' )
+        );
+  }
+
+  init(tr) {
+    this.clearNewTodoLabelInputChannel.init(tr);
+    return this;
+  }
+
+  createTodosChannel(todos) {
+    return new Transmitter.Channels.SimpleChannel()
+    .inBackwardDirection()
+    .fromSources(this.newTodoLabelInputVar, this.newTodoKeypressEvt)
+    .toTarget(todos.todoList)
+    .withTransform(
+      ([labelPayload, keypressPayload], tr) =>
+        labelPayload
+          .replaceByNoop(matchEnter(keypressPayload))
+          .map( (label) => todos.create().init(tr, {label}) )
+          .toAppendListElement()
+    );
+  }
+}
